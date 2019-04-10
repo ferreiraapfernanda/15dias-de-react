1083521717173ec37380bddb312ebf122700e9a0
Fernanda Aparecida Ferreira <ferreira.ap.fernanda@gmail.com>
2019-04-10 20:46:01 -0300

    Trocar o App de um componente stateful por um stateless

--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
 import React, { Component } from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <h1>Hello World React!</h1>
-    );
-  }
-}
+const App = () => <h1>Hello World React 2</h1>
 
 export default App;
