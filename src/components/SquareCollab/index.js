f3192f7c5aad7b21af8d2e0e25936d3d9ebd0f87
Fernanda Aparecida Ferreira <ferreira.ap.fernanda@gmail.com>
2019-04-10 21:04:50 -0300

    Volta a utilizar constante para o componente

---
+++ src/components/SquareCollab/index.js
@@ -0,0 +1,25 @@
+// Maneira mais leve
+import React from "react";
+
+const SquareCollab = () => (
+    <button>X</button>
+);
+
+export default SquareCollab;
+
+
+// Componente com estado
+/*
+import React, {
+    Component
+} from "react";
+
+class SquareCollab extends Component {
+
+    render() {
+        return <button>X</button>
+    }
+}
+
+export default SquareCollab;
+*/
