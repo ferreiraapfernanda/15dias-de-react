175ce4f43ba49aedf3801abb21c6ef19698b8609
Fernanda Aparecida Ferreira <fernanda.ferreira@tegra.com.br>
2019-04-15 13:33:29 -0300

    Adiciona estado no componente e alterna entre X e O no clique do botão

--- src/components/SquareCollab/index.js
+++ src/components/SquareCollab/index.js
@@ -1,12 +1,34 @@
-import React from "react";
+import React, { Component } from "react";
 import "./styles.css";
 
-const handleClick = () => {
-    console.log("FOI")
-}
+class SquareCollab extends Component {
 
-const SquareCollab = () => (
-    <button onClick={handleClick} className="square-collab">X</button>
-);
+    constructor(props) {
+        super(props);
+        this.state = {
+            content: "X"
+        }
+    }
+
+    handleClick = () => {
+        const newState = {};
+
+        if (this.state.content === "X") {
+            newState.content = "O";
+        } else {
+            newState.content = "X";
+        }
+
+        this.setState(newState);
+    }
+
+    render() {
+        return (
+            <button onClick={this.handleClick} className="square-collab">
+                {this.state.content}
+            </button>
+        )
+    }
+}
 
 export default SquareCollab;
