20bdee89474e80f1e582413a25b78445e739a5d5
Fernanda Aparecida Ferreira <fernanda.ferreira@tegra.com.br>
2019-04-16 09:58:41 -0300

    Intermediario: nove squares atraves de uma função que retorna a iteração do map

--- src/components/BoardCollab/index.js
+++ src/components/BoardCollab/index.js
@@ -3,17 +3,14 @@ import React from "react";
 import SquareCollab from "../SquareCollab";
 import "./styles.css";
 
-const BoardCollab = ({ qtd }) => (
+const createSquares = () => {
+    const squares = Array(9).fill();
+    return squares.map((square, index) => <SquareCollab key={index} />)
+}
+
+const BoardCollab = () => (
     <article className="board-collab">
-        <SquareCollab />
-        <SquareCollab />
-        <SquareCollab />
-        <SquareCollab />
-        <SquareCollab />
-        <SquareCollab />
-        <SquareCollab />
-        <SquareCollab />
-        <SquareCollab />
+        {createSquares()}
     </article>
 );
 
